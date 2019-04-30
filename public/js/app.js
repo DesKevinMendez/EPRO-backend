a8a0f1fe41548cdf8382d3740b13707f6aa834ec
Kevin Mendez <yokevinmendez@gmail.com>
2019-04-29 18:02:59 -0600

    Se agrega componente para la visualizacion

--- public/js/app.js
+++ public/js/app.js
@@ -1754,6 +1754,99 @@ module.exports = {
 
 /***/ }),
 
+/***/ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/scanner.vue?vue&type=script&lang=js&":
+/*!******************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib??ref--4-0!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/scanner.vue?vue&type=script&lang=js& ***!
+  \******************************************************************************************************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ __webpack_exports__["default"] = ({
+  name: "Scanner",
+  data: function data() {
+    return {
+      scanner: null,
+      activeCameraId: null,
+      cameras: [],
+      scans: []
+    };
+  },
+  mounted: function mounted() {
+    var self = this;
+    self.scanner = new Instascan.Scanner({
+      video: document.getElementById("preview"),
+      scanPeriod: 5
+    });
+    self.scanner.addListener("scan", function (content, image) {
+      self.scans.unshift({
+        date: +Date.now(),
+        content: content
+      });
+      console.log(content);
+    });
+    Instascan.Camera.getCameras().then(function (cameras) {
+      self.cameras = cameras;
+
+      if (cameras.length > 0) {
+        self.activeCameraId = cameras[0].id;
+        self.scanner.start(cameras[0]);
+      } else {
+        console.error("No cameras found.");
+      }
+    })["catch"](function (e) {
+      console.error(e);
+    });
+  },
+  methods: {
+    formatName: function formatName(name) {
+      return name || "(unknown)";
+    },
+    selectCamera: function selectCamera(camera) {
+      this.activeCameraId = camera.id;
+      this.scanner.start(camera);
+    }
+  }
+});
+
+/***/ }),
+
 /***/ "./node_modules/bootstrap/dist/js/bootstrap.js":
 /*!*****************************************************!*\
   !*** ./node_modules/bootstrap/dist/js/bootstrap.js ***!
@@ -37028,6 +37121,232 @@ exports.clearImmediate = (typeof self !== "undefined" && self.clearImmediate) ||
 
 /***/ }),
 
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/scanner.vue?vue&type=template&id=0d9b4883&":
+/*!**********************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./resources/js/components/scanner.vue?vue&type=template&id=0d9b4883& ***!
+  \**********************************************************************************************************************************************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "render", function() { return render; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return staticRenderFns; });
+var render = function() {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c("div", { staticClass: "container row" }, [
+    _c("div", { staticClass: "sidebar col-md-3" }, [
+      _c("section", { staticClass: "cameras" }, [
+        _c("h2", [_vm._v("Cámaras")]),
+        _vm._v(" "),
+        _c(
+          "ul",
+          [
+            _vm.cameras.length === 0
+              ? _c("li", { staticClass: "empty" }, [
+                  _vm._v("No se encontraron camarás")
+                ])
+              : _vm._e(),
+            _vm._v(" "),
+            _vm._l(_vm.cameras, function(camera) {
+              return _c("li", { key: camera }, [
+                camera.id == _vm.activeCameraId
+                  ? _c(
+                      "span",
+                      {
+                        staticClass: "active",
+                        attrs: { title: _vm.formatName(camera.name) }
+                      },
+                      [_vm._v(_vm._s(_vm.formatName(camera.name)))]
+                    )
+                  : _vm._e(),
+                _vm._v(" "),
+                camera.id != _vm.activeCameraId
+                  ? _c(
+                      "span",
+                      { attrs: { title: _vm.formatName(camera.name) } },
+                      [
+                        _c(
+                          "a",
+                          {
+                            on: {
+                              click: function($event) {
+                                $event.stopPropagation()
+                                return _vm.selectCamera(camera)
+                              }
+                            }
+                          },
+                          [_vm._v(_vm._s(_vm.formatName(camera.name)))]
+                        )
+                      ]
+                    )
+                  : _vm._e()
+              ])
+            })
+          ],
+          2
+        )
+      ]),
+      _vm._v(" "),
+      _c(
+        "section",
+        { staticClass: "scans" },
+        [
+          _c("h2", [_vm._v("Escaneos")]),
+          _vm._v(" "),
+          _vm.scans.length === 0
+            ? _c("ul", [
+                _c("li", { staticClass: "empty" }, [
+                  _vm._v("No hay escaneos todavía")
+                ])
+              ])
+            : _vm._e(),
+          _vm._v(" "),
+          _c(
+            "transition-group",
+            { attrs: { name: "scans", tag: "ul" } },
+            _vm._l(_vm.scans, function(scan) {
+              return _c(
+                "li",
+                { key: scan.date, attrs: { title: scan.content } },
+                [_vm._v(_vm._s(scan.content))]
+              )
+            }),
+            0
+          )
+        ],
+        1
+      )
+    ]),
+    _vm._v(" "),
+    _vm._m(0)
+  ])
+}
+var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "preview-container col-md-9" }, [
+      _c("video", { attrs: { id: "preview" } })
+    ])
+  }
+]
+render._withStripped = true
+
+
+
+/***/ }),
+
+/***/ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js":
+/*!********************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/runtime/componentNormalizer.js ***!
+  \********************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return normalizeComponent; });
+/* globals __VUE_SSR_CONTEXT__ */
+
+// IMPORTANT: Do NOT use ES2015 features in this file (except for modules).
+// This module is a runtime utility for cleaner component module output and will
+// be included in the final webpack user bundle.
+
+function normalizeComponent (
+  scriptExports,
+  render,
+  staticRenderFns,
+  functionalTemplate,
+  injectStyles,
+  scopeId,
+  moduleIdentifier, /* server only */
+  shadowMode /* vue-cli only */
+) {
+  // Vue.extend constructor export interop
+  var options = typeof scriptExports === 'function'
+    ? scriptExports.options
+    : scriptExports
+
+  // render functions
+  if (render) {
+    options.render = render
+    options.staticRenderFns = staticRenderFns
+    options._compiled = true
+  }
+
+  // functional template
+  if (functionalTemplate) {
+    options.functional = true
+  }
+
+  // scopedId
+  if (scopeId) {
+    options._scopeId = 'data-v-' + scopeId
+  }
+
+  var hook
+  if (moduleIdentifier) { // server build
+    hook = function (context) {
+      // 2.3 injection
+      context =
+        context || // cached call
+        (this.$vnode && this.$vnode.ssrContext) || // stateful
+        (this.parent && this.parent.$vnode && this.parent.$vnode.ssrContext) // functional
+      // 2.2 with runInNewContext: true
+      if (!context && typeof __VUE_SSR_CONTEXT__ !== 'undefined') {
+        context = __VUE_SSR_CONTEXT__
+      }
+      // inject component styles
+      if (injectStyles) {
+        injectStyles.call(this, context)
+      }
+      // register component module identifier for async chunk inferrence
+      if (context && context._registeredComponents) {
+        context._registeredComponents.add(moduleIdentifier)
+      }
+    }
+    // used by ssr in case component is cached and beforeCreate
+    // never gets called
+    options._ssrRegister = hook
+  } else if (injectStyles) {
+    hook = shadowMode
+      ? function () { injectStyles.call(this, this.$root.$options.shadowRoot) }
+      : injectStyles
+  }
+
+  if (hook) {
+    if (options.functional) {
+      // for template-only hot-reload because in that case the render fn doesn't
+      // go through the normalizer
+      options._injectStyles = hook
+      // register for functioal component in vue file
+      var originalRender = options.render
+      options.render = function renderWithStyleInjection (h, context) {
+        hook.call(context)
+        return originalRender(h, context)
+      }
+    } else {
+      // inject component registration as beforeCreate hook
+      var existing = options.beforeCreate
+      options.beforeCreate = existing
+        ? [].concat(existing, hook)
+        : [hook]
+    }
+  }
+
+  return {
+    exports: scriptExports,
+    options: options
+  }
+}
+
+
+/***/ }),
+
 /***/ "./node_modules/vue/dist/vue.common.dev.js":
 /*!*************************************************!*\
   !*** ./node_modules/vue/dist/vue.common.dev.js ***!
@@ -49067,48 +49386,9 @@ module.exports = function(module) {
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
 window.Vue = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.common.js");
+Vue.component('Scanner', __webpack_require__(/*! ./components/scanner.vue */ "./resources/js/components/scanner.vue")["default"]);
 var app = new Vue({
-  el: '#app',
-  data: {
-    scanner: null,
-    activeCameraId: null,
-    cameras: [],
-    scans: []
-  },
-  mounted: function mounted() {
-    var self = this;
-    self.scanner = new Instascan.Scanner({
-      video: document.getElementById('preview'),
-      scanPeriod: 5
-    });
-    self.scanner.addListener('scan', function (content, image) {
-      self.scans.unshift({
-        date: +Date.now(),
-        content: content
-      });
-    });
-    Instascan.Camera.getCameras().then(function (cameras) {
-      self.cameras = cameras;
-
-      if (cameras.length > 0) {
-        self.activeCameraId = cameras[0].id;
-        self.scanner.start(cameras[0]);
-      } else {
-        console.error('No cameras found.');
-      }
-    })["catch"](function (e) {
-      console.error(e);
-    });
-  },
-  methods: {
-    formatName: function formatName(name) {
-      return name || '(unknown)';
-    },
-    selectCamera: function selectCamera(camera) {
-      this.activeCameraId = camera.id;
-      this.scanner.start(camera);
-    }
-  }
+  el: '#app'
 });
 
 /***/ }),
@@ -49168,6 +49448,75 @@ if (token) {
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     encrypted: true
 // });
+
+/***/ }),
+
+/***/ "./resources/js/components/scanner.vue":
+/*!*********************************************!*\
+  !*** ./resources/js/components/scanner.vue ***!
+  \*********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _scanner_vue_vue_type_template_id_0d9b4883___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scanner.vue?vue&type=template&id=0d9b4883& */ "./resources/js/components/scanner.vue?vue&type=template&id=0d9b4883&");
+/* harmony import */ var _scanner_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./scanner.vue?vue&type=script&lang=js& */ "./resources/js/components/scanner.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
+
+
+
+
+/* normalize component */
+
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _scanner_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
+  _scanner_vue_vue_type_template_id_0d9b4883___WEBPACK_IMPORTED_MODULE_0__["render"],
+  _scanner_vue_vue_type_template_id_0d9b4883___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+  false,
+  null,
+  null,
+  null
+  
+)
+
+/* hot reload */
+if (false) { var api; }
+component.options.__file = "resources/js/components/scanner.vue"
+/* harmony default export */ __webpack_exports__["default"] = (component.exports);
+
+/***/ }),
+
+/***/ "./resources/js/components/scanner.vue?vue&type=script&lang=js&":
+/*!**********************************************************************!*\
+  !*** ./resources/js/components/scanner.vue?vue&type=script&lang=js& ***!
+  \**********************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_scanner_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib??ref--4-0!../../../node_modules/vue-loader/lib??vue-loader-options!./scanner.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/scanner.vue?vue&type=script&lang=js&");
+/* empty/unused harmony star reexport */ /* harmony default export */ __webpack_exports__["default"] = (_node_modules_babel_loader_lib_index_js_ref_4_0_node_modules_vue_loader_lib_index_js_vue_loader_options_scanner_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
+/***/ "./resources/js/components/scanner.vue?vue&type=template&id=0d9b4883&":
+/*!****************************************************************************!*\
+  !*** ./resources/js/components/scanner.vue?vue&type=template&id=0d9b4883& ***!
+  \****************************************************************************/
+/*! exports provided: render, staticRenderFns */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_scanner_vue_vue_type_template_id_0d9b4883___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../node_modules/vue-loader/lib??vue-loader-options!./scanner.vue?vue&type=template&id=0d9b4883& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js?!./node_modules/vue-loader/lib/index.js?!./resources/js/components/scanner.vue?vue&type=template&id=0d9b4883&");
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "render", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_scanner_vue_vue_type_template_id_0d9b4883___WEBPACK_IMPORTED_MODULE_0__["render"]; });
+
+/* harmony reexport (safe) */ __webpack_require__.d(__webpack_exports__, "staticRenderFns", function() { return _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_scanner_vue_vue_type_template_id_0d9b4883___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"]; });
+
+
 
 /***/ }),
 
